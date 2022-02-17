379ebf23f273bf68335fa951ce2039e5971dcacd
Conor ladrigan <conorladrigan95@gmail.com>
2022-02-17 10:47:28 +0000

    continued development of user class. Added pass through methods

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import { Eventing } from './Eventing';
 import { Attributes } from './Attributes';
 import { Sync } from './Sync';
+import { AxiosResponse } from 'axios';
 
 export interface UserProps {
   name?: string;
@@ -17,5 +18,32 @@ export class User {
 
   constructor(attrs: UserProps) {
     this.attributes = new Attributes<UserProps>(attrs);
+  }
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+    if (typeof id !== 'number') {
+      throw new Error('cannot fetch without an id');
+    }
+    this.sync.fetch(id).then((res: AxiosResponse): void => {
+      this.set(res.data);
+    });
   }
 }
